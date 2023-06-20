d89acd97d90a36223246cd499b6ebd6b8786473e
Dustin Black <dblack@redhat.com>
2023-06-20 16:09:09 +0200

    cleanup after revert

--- tests/test_common.go
+++ tests/test_common.go
@@ -51,7 +51,7 @@ func pullModule(python wrapper.CliWrapper, module string, workDir string, t *tes
 	return nil
 }
 
-func getCliWrapper(t *testing.T, overrideCompatibilityCheck bool, workdir string) wrapper.CliWrapper {
+func getCliWrapper(t *testing.T, workdir string) wrapper.CliWrapper {
 	workDir := workdir
 	pythonPath := "/usr/bin/python3.9"
 	logger := log.NewTestLogger(t)
